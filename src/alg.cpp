6166dea4f3bb374948a3649ec7b6d81a1c98fb2c
RomaZamyatin <78902391+RomaZamyatin@users.noreply.github.com>
2021-03-28 17:05:07 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int count=0;
-	for (int i = 0; i <size; i++)
-	{
-		if (arr[i] == value)
-		count++;
-	}
-	return count;
+   int count=0;
+   for (int i = 0; i <size; i++)
+   {
+       if (arr[i] == value)
+       count++;
+   }
+   return count;
 }
